872b1c945d21be3b64405f59b91749017791df62
DESKTOP-GU2A848\Grażyna <grazyna.kopinska@pg.edu.pl>
2026-06-27 18:13:45 +0200

    Policzone legendarne pokemony

--- cwiczenia_pokemon.py
+++ cwiczenia_pokemon.py
@@ -11,16 +11,16 @@ df_pokemony = pd.read_csv("pokemon_lok.csv")
 # print('----------------- a -------------------')
 # print(df_pokemony['Name'].head(5))
 # print(df_pokemony.shape)
-# print (df_pokemony.sort_values("Name").head(5))
-# Jaka jest Szybkość Pokemona z najwyższym Atakiem?
-# print ("Szybkość Pokemona z najwyższym Atakiem ",
-# #        df_pokemony.sort_values("Attack", ascending = False)["Speed"].head(1))
 # print('------------------ b ----- od Copilota ---')
 # print( df_pokemony.loc[df_pokemony["Attack"].idxmax(), "Speed"])
-print('------------------ c ----- Najwyższe Attack---')
-max_Attack_row = df_pokemony.sort_values("Attack", ascending = False).head(1)
-print('------------------ d ----- Najwyższe Attack---')
-max_Attack_row = df_pokemony.sort_values("Attack").tail(1)
-print (max_Attack_row)
-print('------------------ e ----- Najwyższe Attack---')
-print(max_Attack_row)
+# print('------------------ c ----- Najwyższe Attack---')
+# max_Attack_row = df_pokemony.sort_values("Attack", ascending = False).head(1)
+# print (max_Attack_row)
+# # print('------------------ d ----- Najwyższe Attack---')
+# # row =  df_pokemony.sort_values("Attack").tail(1)
+# print(row[["Attack","Name","Speed"]])
+
+
+## Ile jest legendarnych pokemonów?
+ile = (df_pokemony[df_pokemony['Legendary']==True]).sum()
+print("legendarnych pokemonów jest ",ile["Legendary"])
